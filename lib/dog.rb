12c105b1c9ac17dc501d6dc53fbaf929beba9047
Cliff Jurgensen <jivefox.002@gmail.com>
2018-12-14 15:52:31 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(some_breed)
+    @breed = some_breed
+  end
+  
+  def breed
+    @breed
+  end
+end
+
